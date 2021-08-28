139f0a28d72f0c263859d802a0adab35f0d6eb52
Thom Bruce <thom@thombruce.com>
2021-08-28 01:59:06 +0100

    Temporary App.vue template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <template lang="pug">
-  #app
-    #nav
-      router-link(to="/") Home
-      router-link(to="/about") About
-    router-view
+#app.drawer.drawer-mobile
+  input#main-menu.drawer-toggle(type='checkbox' v-model="showMainMenu")
+
+  .flex-grow.block.overflow-x-hidden.bg-base-100.text-base-content.drawer-content
+    .sticky.inset-x-0.top-0.z-50.w-full.py-1.transition.duration-200.ease-in-out.border-b.border-base-200.bg-neutral.text-neutral-content
+      .mx-auto.space-x-1.navbar.max-w-none
+        .flex-none
+          label.btn.btn-square.btn-ghost.drawer-button(for='main-menu' class='lg:hidden')
+            // fa(:icon='faBars')
+            | M
+        .flex-1
+        .navbar-end
+          // TntUIThemeToggle
+    main.max-w-prose.mx-auto
+      // TntUIBreadcrumbs
+      RouterView
+
+  .drawer-side
+    label.drawer-overlay(for='main-menu')
+    aside.flex.flex-col.justify-between.border-r.border-base-200.bg-base-100.text-base-content.w-80
+      .sticky.inset-x-0.top-0.z-50.hidden.w-full.py-1.transition.duration-200.ease-in-out.border-b.border-base-200.bg-neutral.text-neutral-content(class='lg:block')
+        header.mx-auto.space-x-1.navbar.max-w-none
+          .navbar-start
+            NuxtLink.btn.btn-ghost(to='/')
+              span.text-lg.font-bold Title
+      div.h-full
+        ul.menu.flex.flex-col.p-4
+          li.menu-title.mt-2
+            span
+              RouterLink(to='/') Home
+          li
+            RouterLink(to='/about') About
 </template>
 
 <script>
@@ -12,29 +39,11 @@ export default {
     htmlAttrs: {
       'data-theme': 'cupcake'
     }
+  },
+  data () {
+    return {
+      showMainMenu: false
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
